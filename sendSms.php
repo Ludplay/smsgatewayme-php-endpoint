cd8121af76eb1ab274e10eed9f73776b856acd02
Ludplay <34750574+Ludplay@users.noreply.github.com>
2018-05-21 16:27:14 -0300

    Create sendSms.php

---
+++ sendSms.php
@@ -0,0 +1,17 @@
+```
+<?php
+
+require_once('smsGatewayV4.php');
+$token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+$phone = "+5531999999999";
+$message = "Test smsGatewayV4";
+$deviceID = 00000;
+$options = [];
+
+$smsGateway = new SmsGateway($token);
+$result = $smsGateway->sendMessageToNumber($tel_destinatario, $message, $deviceID, $options);
+
+print_r($result);
+?>
+```
